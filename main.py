b233b6cdf2b9bfdecabd7eb6b850feb115b7d487
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-12-02 21:24:05 -0500

    Loaded data using tf

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from sklearn.model_selection import train_test_split
 import os
 import cv2
 import imghdr
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-#Create dataset with images:
+#Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_logical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+#Create data directory with images:
 data_dir = 'PetImages/Train'
 image_exts = ['jpeg', 'jpg', 'bmp', 'png']
 
@@ -29,3 +35,26 @@ for image_class in os.listdir(data_dir):
 
 #Load Data
 tf.data.Dataset
+
+#Builds data pipeline
+data = tf.keras.utils.image_dataset_from_directory('PetImages/Train')
+
+#Allows us to access data pipeline
+data_iterator = data.as_numpy_iterator()
+
+#Accessing data pipeline itself
+batch = data_iterator.next()
+
+#Class 1 = Dog
+#Class 0 = Cat
+fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+
+#2.Preprocess data
+
+#2.1 Scale Data
+data = data.map(lambda x,y: (x/255, y))
+
+
